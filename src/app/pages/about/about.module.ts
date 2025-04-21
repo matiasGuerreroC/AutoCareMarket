063c05bf3e4ede1ab921abab3a21e09fd3600ece
Matias Guerrero Castillo <matias.guerrero.c@mail.pucv.cl>
2025-04-21 00:05:59 -0400

    feat(layout): crear componentes Header y Footer reutilizables + agregar portada en Home
    - Se crean los componentes `HeaderComponent` y `FooterComponent` con diseño basado en el mockup
    - Se implementa un `SharedModule` para declarar y exportar los componentes compartidos
    - Se actualizan las páginas `Home` y `About` para incluir `app-header` y `app-footer`
    - Se añade banner de portada en la página Home para diferenciar visualmente las vistas

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { AboutPageRoutingModule } from './about-routing.module';
 
 import { AboutPage } from './about.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     AboutPageRoutingModule
   ],
   declarations: [AboutPage]
